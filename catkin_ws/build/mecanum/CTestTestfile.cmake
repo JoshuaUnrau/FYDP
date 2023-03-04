45e1ff071e065acadc927682832170d663801289
jpunr <jpunrau@gmail.com>
2023-03-03 19:49:37 -0500

    Added workspace

--- catkin_ws/build/mecanum/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/fydp/catkin_ws/src/mecanum
-# Build directory: /home/fydp/catkin_ws/build/mecanum
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- catkin_ws/build/pwm/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/fydp/catkin_ws/src/pwm
-# Build directory: /home/fydp/catkin_ws/build/pwm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
